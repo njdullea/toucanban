be94a9c1ffba686de78f7642cb24cc637fe19faf
Nathan Dullea <njdullea@gmail.com>
2021-04-01 11:50:06 -0500

    added transition to the popup settings modal

--- src/components/settingsModal.tsx
+++ src/components/settingsModal.tsx
@@ -3,6 +3,7 @@ import { Card, Text, Divider, Box, Button, useColorMode, useThemeUI } from 'them
 import { Sun, User, Users, Activity } from 'react-feather';
 import OutsideClickHandler from 'react-outside-click-handler';
 import { Settings } from 'react-feather';
+import { animated, useSpring } from 'react-spring';
 
 function SettingsModal() {
   const [colorMode, setColorMode] = useColorMode();
@@ -10,8 +11,15 @@ function SettingsModal() {
   const context = useThemeUI();
   const { theme } = context;
 
+  const animateProps = useSpring({ opacity: displaySettingsModal ? 1 : 0 });
+
   return (
     <Box>
+      <OutsideClickHandler
+        onOutsideClick={() => {
+          setDisplaySettingsModal(false);
+        }}
+      >
       <Button
         onClick={() => setDisplaySettingsModal(!displaySettingsModal)}
         bg={theme.colors?.muted as string}
@@ -20,46 +28,43 @@ function SettingsModal() {
       >
         <Settings />
       </Button>
-      { displaySettingsModal &&
-        <Box sx={{position: 'absolute', top: 100, right: 30}}> 
-          <OutsideClickHandler
-            onOutsideClick={() => {
-              setDisplaySettingsModal(false);
-            }}
-          >
-            <Card sx={{
-              width: 256,
-              padding: 2,
-              backgroundColor: 'muted',
-              color: 'black'
-            }}>
-              <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
-                <Text>Profile</Text>
-                <User />
-              </Button>
-              <Divider />
-              <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
-                <Text>Organizations</Text>
-                <Users />
-              </Button>
-              <Divider />
-              <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
-                <Text>Projects</Text>
-                <Activity />
-              </Button>
-              <Divider />
-              <Button
-                bg={'muted'}
-                sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}
-                onClick={() => setColorMode(colorMode === 'default' ? 'dark' : 'default')}
-              >
-                <Text>Color Mode</Text>
-                <Sun />
-              </Button>
-            </Card>
-          </OutsideClickHandler>
-        </Box>
-      }
+        <animated.div style={animateProps}>
+          <Box sx={{position: 'absolute', top: 100, right: 30}}> 
+            
+              <Card sx={{
+                width: 256,
+                padding: 2,
+                backgroundColor: 'muted',
+                color: 'black'
+              }}>
+                <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
+                  <Text>Profile</Text>
+                  <User />
+                </Button>
+                <Divider />
+                <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
+                  <Text>Organizations</Text>
+                  <Users />
+                </Button>
+                <Divider />
+                <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
+                  <Text>Projects</Text>
+                  <Activity />
+                </Button>
+                <Divider />
+                <Button
+                  bg={'muted'}
+                  sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}
+                  onClick={() => setColorMode(colorMode === 'default' ? 'dark' : 'default')}
+                >
+                  <Text>Color Mode</Text>
+                  <Sun />
+                </Button>
+              </Card>
+            
+          </Box>
+        </animated.div>
+      </OutsideClickHandler>
     </Box>
   )
 };
