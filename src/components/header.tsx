5d099518172959b9d2507a73d4bfb0dbb67b0e9d
Nathan Dullea <njdullea@gmail.com>
2021-04-01 10:18:36 -0500

    moved all settings modal functionality to settings component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
-import { Box, Heading, useColorMode, Button, useThemeUI } from 'theme-ui';
+import { Box, Heading, useColorMode, useThemeUI } from 'theme-ui';
 import ToucanIcon from './icon';
-import { Settings } from 'react-feather';
 import SettingsModal from './settingsModal';
-import OutsideClickHandler from 'react-outside-click-handler';
 
 function Header() {
   const [colorMode] = useColorMode();
-  const [displaySettingsModal, setDisplaySettingsModal] = useState(false);
   const context = useThemeUI();
   const { theme } = context;
 
@@ -21,28 +17,7 @@ function Header() {
         />
         <Heading as='h1' pl={3}>Toucanban</Heading>
       </Box>
-      <Button
-        onClick={() => setDisplaySettingsModal(!displaySettingsModal)}
-        bg={theme.colors?.muted as string}
-        p={0}
-        sx={{ display: 'flex', height: '48px', width: '48px', borderRadius: '8px', alignItems: 'center', justifyContent: 'center' }}
-      >
-        <Settings />
-        {displaySettingsModal &&
-          <OutsideClickHandler
-            onOutsideClick={() => {
-            setDisplaySettingsModal(false);
-            }}
-          >
-          <Box
-            sx={{position: 'absolute', top: 100, right: 30}}  
-          >
-            
-            <SettingsModal />
-          </Box>
-          </OutsideClickHandler>
-        }
-      </Button>
+      <SettingsModal />
     </Box>
   );
 }
--- src/components/settingsModal.tsx
+++ src/components/settingsModal.tsx
@@ -1,41 +1,66 @@
-import React from 'react';
-import { Card, Text, Divider, Box, Button, useColorMode } from 'theme-ui';
+import React, {useState} from 'react';
+import { Card, Text, Divider, Box, Button, useColorMode, useThemeUI } from 'theme-ui';
 import { Sun, User, Users, Activity } from 'react-feather';
+import OutsideClickHandler from 'react-outside-click-handler';
+import { Settings } from 'react-feather';
 
 function SettingsModal() {
   const [colorMode, setColorMode] = useColorMode();
+  const [displaySettingsModal, setDisplaySettingsModal] = useState(false);
+  const context = useThemeUI();
+  const { theme } = context;
 
   return (
-    <Card sx={{
-      width: 256,
-      padding: 2,
-      backgroundColor: 'muted',
-      color: 'black'
-    }}>
-      <Button bg={'muted'} sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}} p={1}>
-        <Text>Profile</Text>
-        <User />
-      </Button>
-      <Divider />
-      <Button bg={'muted'} sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}} p={1}>
-        <Text>Organizations</Text>
-        <Users />
-      </Button>
-      <Divider />
-      <Button bg={'muted'} sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}} p={1}>
-        <Text>Projects</Text>
-        <Activity />
-      </Button>
-      <Divider />
+    <Box>
       <Button
-        bg={'muted'}
-        sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}
-        onClick={() => setColorMode(colorMode === 'default' ? 'dark' : 'default')}
+        onClick={() => setDisplaySettingsModal(!displaySettingsModal)}
+        bg={theme.colors?.muted as string}
+        p={0}
+        sx={{ display: 'flex', height: '48px', width: '48px', borderRadius: '8px', alignItems: 'center', justifyContent: 'center' }}
       >
-        <Text>Color Mode</Text>
-        <Sun />
+        <Settings />
       </Button>
-    </Card>
+      { displaySettingsModal &&
+        <Box sx={{position: 'absolute', top: 100, right: 30}}> 
+          <OutsideClickHandler
+            onOutsideClick={() => {
+              setDisplaySettingsModal(false);
+            }}
+          >
+            <Card sx={{
+              width: 256,
+              padding: 2,
+              backgroundColor: 'muted',
+              color: 'black'
+            }}>
+              <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
+                <Text>Profile</Text>
+                <User />
+              </Button>
+              <Divider />
+              <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
+                <Text>Organizations</Text>
+                <Users />
+              </Button>
+              <Divider />
+              <Button bg={'muted'} sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}>
+                <Text>Projects</Text>
+                <Activity />
+              </Button>
+              <Divider />
+              <Button
+                bg={'muted'}
+                sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} p={1}
+                onClick={() => setColorMode(colorMode === 'default' ? 'dark' : 'default')}
+              >
+                <Text>Color Mode</Text>
+                <Sun />
+              </Button>
+            </Card>
+          </OutsideClickHandler>
+        </Box>
+      }
+    </Box>
   )
 };
 
