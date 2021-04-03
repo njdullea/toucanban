18abcc78f56c0a40e0f6a65ceac85bf03c3675ec
Nathan Dullea <njdullea@gmail.com>
2021-04-02 19:22:16 -0500

    added item placeholders

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -5,7 +5,46 @@ import { ArrowLeft, ArrowRight, PlusCircle } from 'react-feather';
 import ItemTypes from './itemTypes';
 import AddItem from './addItem';
 
+// The date and time in: "yyyy-MM-dd'T'HH:mm:ss.SSSxxx" (using date fns). The is the same as the input for parsing a date: https://javascript.info/date#date-parse-from-a-string.
+type dateTime = string;
+
+interface item {
+  id: string,
+  description: string,
+  // The time the task starts.
+  startDateTime: dateTime,
+  // The time the task should end. 
+  endDateTime: dateTime | undefined,
+  // The time the user marked the task as complete.
+  confirmedEndDateTime: dateTime | undefined,
+  // If its a meeting, we shouldn't have to confirm complete. If it is a task, we should.
+  continueUntilConfirmed: boolean,
+}
+
 function ProjectInfo() {
+  const [items, setItems] = useState([
+    {
+      id: '1',
+      description: 'Setup Project',
+      startDateTime: '2021-04-02T09:00:00.000-07:00',
+      continueUntilConfirmed: true,
+    },
+    {
+      id: '2',
+      description: 'Meet about Project Overview',
+      startDateTime: '2021-04-02T13:00:00.000-07:00',
+      endDateTime: '2021-04-02T14:00:00.000-07:00',
+      continueUntilConfirmed: false,
+    },
+    {
+      id: '2',
+      description: 'Begin Prototype Implementation',
+      startDateTime: '2021-04-02T15:00:00.000-07:00',
+      endDateTime: '2021-04-04T09:00:00.000-07:00',
+      continueUntilConfirmed: false,
+    },
+  ]);
+
   const [itemTypes, setItemTypes] = useState([
     {
       id: '1',
@@ -22,6 +61,14 @@ function ProjectInfo() {
       name: 'Review',
       color: '#3f51b5'
     },
+    // {
+    //   id: '4',
+    //   name: 'Meeting',
+    //   color: '#42c5f5',
+    // For things like meetings they have a set start and date time, and we don't want to both users with clocking them.
+    // For other tasks we need them to track it, or we can just have estimated end or actual end?
+    //   fixedTiming: false
+    // }
   ]);
 
   const getStartingDisplayDates = () => {
